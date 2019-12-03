5bdd735358281be3dc095e2750bcc751696e4661
chaitanya <ckhode@gmail.com>
2019-12-03 18:42:52 +0530

    initial app

---
+++ src/app/product.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  url ='http://172.18.0.181:9898/product'
+  constructor(private http : HttpClient) { }
+
+  getProduct()
+    {
+      return this.http.get(this.url)
+    }
+}
+
+
